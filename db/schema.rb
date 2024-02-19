19389bec2487975bedacd9b5c6ad1655cf1eb871
Fabrice Tuilliere <fabrice.tui@gmail.com>
2024-02-19 21:18:21 +0100

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,60 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_230556) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_19_201637) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "shops", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "phone"
-    t.string "email"
-    t.string "url"
-    t.text "caption"
-    t.string "facebook"
-    t.string "instagram"
-    t.string "twitter"
-    t.string "linkedin"
-    t.string "global_opening"
-    t.string "monday_opening"
-    t.string "tuesday_opening"
-    t.string "wednesday_opening"
-    t.string "thursday_opening"
-    t.string "friday_opening"
-    t.string "saturday_opening"
-    t.string "sunday_opening"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -73,25 +22,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_230556) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
-    t.string "phone"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "name"
-    t.boolean "admin", default: false
-    t.boolean "artist", default: false
-    t.string "speciality"
-    t.text "description"
-    t.string "start_time", default: "00:00"
-    t.string "end_time", default: "00:00"
-    t.string "facebook"
-    t.string "twitter"
-    t.string "instagram"
-    t.string "linkedin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
